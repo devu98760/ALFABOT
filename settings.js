1cd2bc6ed27af26a148c5fbed73f307b646c4a91
devu98760 <160774935+devu98760@users.noreply.github.com>
2024-02-22 14:24:21 +0530

    settings.js

--- settings.js
+++ settings.js
@@ -23,13 +23,13 @@ global.doc6 = 'application/vnd.android.package-archive'
 //owmner v card
 global.owner = ['916909137213'] //ur owner number
 global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.ownername = "🦄DEVU" //ur owner name
+global.ytname = "YT: ALFA" //ur yt chanel name
+global.socialm = "GitHub: devu98760" //ur github or insta name
+global.location = "India, Kerala, Kollam" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
+global.botname = "Alfa"
 global.ownernumber = '916909137213'
 global.ownername = '🦄드림 가이 Xeon'
 global.ownerNumber = ["916909137213@s.whatsapp.net"]
@@ -37,8 +37,8 @@ global.ownerweb = "https://youtube.com/@DGXeon"
 global.websitex = "https://youtu.be/L_SIk59QeAU"
 global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
+global.wm = "ALFA Bot Inc."
+global.botscript = 'https://github.com/devu98760/ALFABOT.git' //script link
 global.packname = "Sticker By"
 global.author = "🦄드림 가이 Xeon\n\n"
 global.creator = "916909137213@s.whatsapp.net"
